81e60ad1b9fb6664050cbc8c2e63e085dd0efbb9
nikolay_dimitrov256 <raystlin7@gmail.com>
2025-06-01 23:11:56 +0300

    Worked on the about page

--- js/about.js
+++ js/about.js
@@ -5,17 +5,30 @@ function aboutElements(e) {
     personalInfoAElements.forEach(element => element.addEventListener('click', (e) => fillContent(e, element)));
 }
 
+// function fillContent(e, aElement) {
+//     e.preventDefault;
+//     const targetPElement = document.getElementById('about-info');
+//     const contentPElement = document.getElementById(aElement.classList[0]);
+//     const labelPElement = document.getElementById('about-label');
+
+//     targetPElement.textContent = contentPElement.textContent;
+//     labelPElement.textContent = contentPElement.id;
+// }
+
 function fillContent(e, aElement) {
     e.preventDefault;
-    const targetPElement = document.getElementById('about-info');
-    const contentPElement = document.getElementById(aElement.classList[0]);
+    const targetDivElement = document.getElementById('about-content');
+    const contentDivElement = document.getElementById(aElement.classList[0]);
     const labelPElement = document.getElementById('about-label');
+    const focusedDivElement = targetDivElement.querySelector('div');
+    const hiddenDivElement = document.querySelector('.hidden-info');
+    
+    labelPElement.textContent = contentDivElement.id;
+    hiddenDivElement.append(focusedDivElement);
+    targetDivElement.append(contentDivElement);
 
-    targetPElement.textContent = contentPElement.textContent;
-    labelPElement.textContent = contentPElement.id;
-}
+    // Unselect the previously seleted element
+    document.querySelector('.about-nav a.selected').classList.remove('selected');
 
-function fillContent(e, aElement) {
-    e.preventDefault;
-    const targetDivElement = document.getElementById('about-content')
+    aElement.classList.add('selected');
 }
